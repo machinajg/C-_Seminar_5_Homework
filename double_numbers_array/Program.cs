6dcd4b475a0a88c3cde7f4d66d9a97d7cdea7dc0
Julia Machina <machinajg@gmail.com>
2023-02-14 16:22:58 +0300

    Double numbers max min array project added

--- double_numbers_array/Program.cs
+++ double_numbers_array/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void GetArray(double[] arr)
+{
+    int length = arr.Length;
+    for (int index = 0; index < length; index++)
+    {
+       arr[index] = new Random().Next(10, 200);
+       Console.Write($"{arr[index]}, ");
+    }
+    Console.WriteLine();
+}
+double GetMaxMin(double[] Numbers)
+{
+    int length = Numbers.Length;
+    double MaxNum = Numbers[0];
+    double MinNum = Numbers [0];
+    double Diff = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (Numbers[i] > MaxNum)  MaxNum = Numbers[i];   
+        else if (Numbers[i] < MinNum)  MinNum = Numbers[i];
+    }
+    Diff = MaxNum - MinNum;
+Console.WriteLine($"разница между max и min элементами в массиве: {Diff}");
+return Diff;
+}
+double[] Numbs = new double[10];
+GetArray(Numbs);
+GetMaxMin(Numbs);
+
